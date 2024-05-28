f5db1102f9ad62a335ae7b1c5e6409b3947463b5
陳宣志 <152285982+codingnewbieTED@users.noreply.github.com>
2024-05-28 21:28:50 +0800

    Add files via upload

---
+++ Lab08/PATTERN/PATTERN.sv
@@ -0,0 +1,528 @@
+/*
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NYCU Institute of Electronic
+2024 Spring IC Design Laboratory 
+Lab08: SystemVerilog Design and Verification 
+File Name   : PATTERN.sv
+Module Name : PATTERN
+Release version : v1.0 (Release Date: Nov-2023)
+Author : Jui-Huang Tsai (erictsai.ee12@nycu.edu.tw)
+//   (C) Copyright Laboratory System Integration and Silicon Implementation
+//   All Right Reserved
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*/
+`ifdef RTL
+
+  `define CYCLE_TIME 12.0
+`endif
+
+`ifdef GATE
+  `define CYCLE_TIME 1.8
+`endif
+`define PATNUM 1000
+`include "Usertype_BEV.sv"
+
+program automatic PATTERN(input clk, INF.PATTERN inf);
+import usertype::*;
+
+//================================================================
+// parameters & integer
+//================================================================
+parameter DRAM_p_r = "../00_TESTBED/DRAM/dram.dat";
+integer i,latency,total_latency,i_pat;
+real CYCLE = `CYCLE_TIME;
+
+//================================================================
+// class random
+//================================================================
+class random_act;
+    randc Action act_id;
+    constraint range{
+        act_id inside{Make_drink, Supply, Check_Valid_Date};
+    }
+endclass
+
+
+
+class random_bev;
+    randc Bev_Type bev_id;
+    constraint range{
+        bev_id inside{Black_Tea,
+                        Milk_Tea,
+                        Extra_Milk_Tea,
+                        Green_Tea,
+                        Green_Milk_Tea,
+                        Pineapple_Juice,
+                        Super_Pineapple_Tea,
+                        Super_Pineapple_Milk_Tea};
+    }
+endclass
+
+class random_size;
+    randc Bev_Size size_id;
+    constraint range{
+        size_id inside{L, M, S};
+    }
+endclass
+
+class random_date;
+    randc Month month_id;
+    randc Day day_id;
+    constraint range1{
+        month_id inside {[1:12]};
+    }
+    constraint range2{
+        (month_id==2) -> day_id inside {[1:28]};
+        (month_id == 4 || month_id == 6 || month_id == 9 || month_id == 11) -> day_id inside {[1:30]};
+        (month_id == 1 || month_id == 3 || month_id == 5 || month_id == 7 || month_id == 8 || month_id == 10 || month_id == 12 ) -> day_id inside{[1:31]};
+    }
+endclass
+
+class random_box;
+    randc Barrel_No box_id;
+    constraint range{
+        box_id inside{[0:255]};
+    }
+endclass
+
+class random_supply;
+    randc ING black_supply;
+    randc ING green_supply;
+    randc ING milk_supply;
+    randc ING pineapple_supply;
+    constraint limit1{
+        black_supply inside{[0:4095]};
+        green_supply inside{[0:4095]};
+        milk_supply  inside{[0:4095]};
+        pineapple_supply inside{[0:4095]};
+    }
+endclass
+
+//================================================================
+// wire & registers 
+//================================================================
+logic [7:0] golden_DRAM [((65536+8*256)-1):(65536+0)];
+//data from golden DRAM
+ING dram_black_tea,dram_green_tea,dram_milk,dram_pineapple;
+Month dram_month;
+Day dram_day;
+// make drink minus ING
+ING make_drink_black,make_drink_green,make_drink_milk,make_drink_pineapple;
+logic [63:0] dram_data;
+logic [63:0] new_dram_data;
+//golden
+logic golden_complete;
+logic [1:0] golden_err_msg;
+//random object
+random_act act = new();            //instance object
+random_bev bev_type = new();
+random_size size = new();
+random_date date_today = new();
+random_box addr_DRAM = new();
+random_supply IND_supply = new();
+//================================================================
+// initial
+//================================================================
+//initial block
+initial $readmemh(DRAM_p_r, golden_DRAM);
+
+initial begin
+    total_latency = 0;
+    reset_task;
+    for(i_pat=0 ; i_pat < `PATNUM ; i_pat=i_pat+1) begin
+        input_task;
+        cal_ans;
+        wait_valid;
+        check_ans;
+        //check_dram;
+        total_latency = total_latency + latency;
+        $display("PASS    PATTERN NO.%4d  | Latency:%4d |address :%4d", i_pat,latency,addr_DRAM.box_id);
+    end
+    $display("Congratulations! Cycle Time:%0.1f ns , Execution cycles: %d" , CYCLE, total_latency);
+    $finish;
+end
+
+//task//
+task reset_task; begin
+    inf.rst_n = 1;
+    inf.sel_action_valid = 0;
+    inf.type_valid = 0;
+    inf.size_valid = 0;
+    inf.date_valid = 0;
+    inf.box_no_valid = 0;
+    inf.box_sup_valid = 0;
+    inf.D = 'bx;
+    force clk = 0;
+    inf.rst_n = 0;
+    #(CYCLE *100);
+
+    if(inf.out_valid !== 0 || inf.err_msg !== 2'b00 || inf.complete !== 0) begin
+        $fatal(1,"reset task");  
+        //$display("reset task");
+        //$finish;
+    end
+    #(CYCLE*5);
+    inf.rst_n = 1;
+    release clk;
+end endtask    
+
+task input_task;begin
+    repeat($random()%3+1) @(negedge clk);
+    inf.sel_action_valid = 1;
+    act.randomize();                   //instance object , and execute its default object function
+    inf.D.d_act[0] = act.act_id;       // object's member 
+    @(negedge clk);
+    inf.sel_action_valid = 0;
+    //inf.D = 'bx;
+
+    if(act.act_id == Make_drink) task_MAKE_DRINK;
+    else if(act.act_id == Supply) task_SUPPLY;
+    else task_Check;
+end endtask
+
+task task_MAKE_DRINK;begin
+    reg [1:0] interval;
+
+    //type input
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);
+    inf.type_valid = 1;
+    bev_type.randomize();
+    inf.D.d_type[0] = bev_type.bev_id;
+    @(negedge clk);
+    inf.type_valid = 0;
+    //inf.D.d_type = bev_type;
+    
+    //size input
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);   
+    inf.size_valid = 1;
+    size.randomize();
+    inf.D.d_size[0] = size.size_id;
+    @(negedge clk);
+    inf.size_valid = 0;
+
+    //date input
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);
+    inf.date_valid = 1;
+    date_today.randomize();
+    inf.D.d_date[0] = {date_today.month_id,date_today.day_id};
+    @(negedge clk);
+    inf.date_valid = 0;
+
+    //box input
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);
+    inf.box_no_valid = 1;
+    addr_DRAM.randomize();
+    inf.D.d_box_no[0] = addr_DRAM.box_id;
+    @(negedge clk);
+    inf.box_no_valid = 0;
+    inf.D = 'bx;
+
+end endtask
+
+task task_SUPPLY;begin
+    reg [1:0] interval;
+
+    //date input
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);
+    inf.date_valid = 1;
+    date_today.randomize();
+    inf.D.d_date[0] = {date_today.month_id,date_today.day_id};
+    @(negedge clk);
+    inf.date_valid = 0;
+    //inf.D.d_type = bev_type;
+    
+    //box input
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);   
+    inf.box_no_valid = 1;
+    addr_DRAM.randomize();
+    inf.D.d_box_no[0] = addr_DRAM.box_id;
+    @(negedge clk);
+    inf.box_no_valid = 0;
+
+    //supply input * 4
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);
+    inf.box_sup_valid = 1;
+    IND_supply.randomize();
+    inf.D.d_ing[0] = IND_supply.black_supply;
+    @(negedge clk);
+    inf.box_sup_valid = 0;
+
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);
+    inf.box_sup_valid = 1;
+    inf.D.d_ing[0] = IND_supply.green_supply;
+    @(negedge clk);
+    inf.box_sup_valid = 0;
+
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);
+    inf.box_sup_valid = 1;
+    inf.D.d_ing[0] = IND_supply.milk_supply;
+    @(negedge clk);
+    inf.box_sup_valid = 0;
+
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);
+    inf.box_sup_valid = 1;
+    inf.D.d_ing[0] = IND_supply.pineapple_supply;
+    @(negedge clk);
+    inf.box_sup_valid = 0;
+    inf.D = 'bx;
+
+end endtask
+
+task task_Check;begin
+    reg [1:0] interval;
+
+    //date input
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);
+    inf.date_valid = 1;
+    date_today.randomize();
+    inf.D.d_date[0] = {date_today.month_id,date_today.day_id};
+    @(negedge clk);
+    inf.date_valid = 0;
+
+    //box input
+    interval = $urandom_range(0,3);
+    repeat(interval)  @(negedge clk);   
+    inf.box_no_valid = 1;
+    addr_DRAM.randomize();
+    inf.D.d_box_no[0] = addr_DRAM.box_id;
+    @(negedge clk);
+    inf.box_no_valid = 0;
+    inf.D = 'bx;
+
+end endtask
+
+task cal_ans; begin
+    //supply minus
+    make_drink_black = 0;
+    make_drink_green = 0;
+    make_drink_milk = 0;
+    make_drink_pineapple = 0;
+    case( {bev_type.bev_id, size.size_id})
+    {Black_Tea,L}: begin
+        make_drink_black = 960;
+    end
+    {Black_Tea,M}: begin
+        make_drink_black = 720;
+    end
+    {Black_Tea,S}: begin
+        make_drink_black = 480;
+    end
+    //black:milk = 3:1
+    {Milk_Tea,L}:begin    
+        make_drink_black = 720;
+        make_drink_milk = 240;
+    end
+    {Milk_Tea,M}:begin    
+        make_drink_black = 540;
+        make_drink_milk = 180;
+    end
+    {Milk_Tea,S}:begin    
+        make_drink_black = 360;
+        make_drink_milk = 120;
+    end
+    //Extra_Milk_Tea  , black:milk = 1:1
+    {Extra_Milk_Tea,L}:begin    
+        make_drink_black = 480;
+        make_drink_milk = 480;
+    end
+    {Extra_Milk_Tea,M}:begin    
+        make_drink_black = 360;
+        make_drink_milk = 360;
+    end
+    {Extra_Milk_Tea,S}:begin    
+        make_drink_black = 240;
+        make_drink_milk = 240;
+    end
+    //Green_Tea ,  green : 1
+    {Green_Tea,L}:begin    
+        make_drink_green = 960;
+    end
+    {Green_Tea,M}:begin    
+        make_drink_green = 720;
+    end
+    {Green_Tea,S}:begin    
+        make_drink_green = 480;
+    end
+    //Green_Milk_Tea
+    {Green_Milk_Tea,L}:begin
+        make_drink_green = 480;
+        make_drink_milk = 480;
+    end
+    {Green_Milk_Tea,M}:begin
+        make_drink_green = 360;
+        make_drink_milk = 360;
+    end
+    {Green_Milk_Tea,S}:begin
+        make_drink_green = 240;
+        make_drink_milk = 240;
+    end
+    {Pineapple_Juice,L}:  make_drink_pineapple = 960;
+    {Pineapple_Juice,M}:  make_drink_pineapple = 720;
+    {Pineapple_Juice,S}:  make_drink_pineapple = 480;
+    {Super_Pineapple_Tea,L}: begin
+        make_drink_black = 480;
+        make_drink_pineapple = 480;
+    end
+    {Super_Pineapple_Tea,M}: begin
+        make_drink_black = 360;
+        make_drink_pineapple = 360;
+    end
+    {Super_Pineapple_Tea,S}: begin
+        make_drink_black = 240;
+        make_drink_pineapple = 240;
+    end
+    {Super_Pineapple_Milk_Tea,L}:begin
+        make_drink_black = 480;
+        make_drink_milk = 240;
+        make_drink_pineapple = 240;
+    end
+    {Super_Pineapple_Milk_Tea,M}:begin
+        make_drink_black = 360;
+        make_drink_milk = 180;
+        make_drink_pineapple = 180;
+    end
+    {Super_Pineapple_Milk_Tea,S}:begin
+        make_drink_black = 240;
+        make_drink_milk = 120;
+        make_drink_pineapple = 120;
+    end
+    default:begin
+        make_drink_black = 0;
+        make_drink_green = 0;
+        make_drink_milk = 0;
+        make_drink_pineapple = 0;
+    end
+    endcase
+
+    dram_data = {golden_DRAM[65536 + addr_DRAM.box_id*8 + 7],golden_DRAM[65536 + addr_DRAM.box_id*8 + 6],golden_DRAM[65536 + addr_DRAM.box_id*8 + 5],golden_DRAM[65536 + addr_DRAM.box_id*8 + 4],golden_DRAM[65536 + addr_DRAM.box_id*8 + 3],golden_DRAM[65536 + addr_DRAM.box_id*8 + 2],golden_DRAM[65536 + addr_DRAM.box_id*8 + 1],golden_DRAM[65536 + addr_DRAM.box_id*8 ]};
+    dram_black_tea = dram_data[63:52];
+    dram_green_tea = dram_data[51:40];
+    dram_month = dram_data[39:32];
+    dram_milk = dram_data[31:20];
+    dram_pineapple = dram_data[19:8];
+    dram_day = dram_data[7:0];
+
+    if(act.act_id == Make_drink) begin
+        if({date_today.month_id,date_today.day_id} > {dram_month[3:0],dram_day[4:0]}) begin
+           golden_complete = 0;
+            golden_err_msg = No_Exp;
+        end    
+        else if(dram_black_tea < make_drink_black || dram_green_tea < make_drink_green || dram_milk < make_drink_milk || dram_pineapple < make_drink_pineapple)begin
+            golden_complete = 0;
+            golden_err_msg = No_Ing;
+        end
+        else begin
+            golden_complete = 1;
+            golden_err_msg = No_Err;
+            new_dram_data[63:52] = dram_black_tea - make_drink_black;
+            new_dram_data[51:40] = dram_green_tea - make_drink_green;
+            new_dram_data[39:32] = dram_data[39:32];
+            new_dram_data[31:20] = dram_milk - make_drink_milk;
+            new_dram_data[19:8]  = dram_pineapple - make_drink_pineapple;
+            new_dram_data[7:0]   = dram_data[7:0];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 7] = new_dram_data[63:56];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 6] = new_dram_data[55:48];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 5] = new_dram_data[47:40];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 4] = new_dram_data[39:32];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 3] = new_dram_data[31:24];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 2] = new_dram_data[23:16];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 1] = new_dram_data[15:8];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 ] = new_dram_data[7:0];
+        end
+    end
+    else if(act.act_id == Supply) begin
+        if( (dram_black_tea + IND_supply.black_supply) > 4095 || (dram_green_tea + IND_supply.green_supply) > 4095 || (dram_milk + IND_supply.milk_supply) > 4095 || (dram_pineapple + IND_supply.pineapple_supply) > 4095) begin
+            golden_complete = 0;
+            golden_err_msg = Ing_OF;
+            new_dram_data[63:52] = ((dram_black_tea + IND_supply.black_supply) > 4095)? 4095 : dram_black_tea + IND_supply.black_supply;
+            new_dram_data[51:40] = ((dram_green_tea + IND_supply.green_supply) > 4095)? 4095 : dram_green_tea + IND_supply.green_supply;
+            new_dram_data[39:32] = date_today.month_id;
+            new_dram_data[31:20] = ((dram_milk + IND_supply.milk_supply) > 4095)? 4095 : dram_milk + IND_supply.milk_supply;
+            new_dram_data[19:8]  = ((dram_pineapple + IND_supply.pineapple_supply) > 4095)? 4095 : dram_pineapple + IND_supply.pineapple_supply;
+            new_dram_data[7:0]   = date_today.day_id;
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 7] = new_dram_data[63:56];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 6] = new_dram_data[55:48];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 5] = new_dram_data[47:40];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 4] = new_dram_data[39:32];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 3] = new_dram_data[31:24];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 2] = new_dram_data[23:16];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 1] = new_dram_data[15:8];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 ] = new_dram_data[7:0];
+        end
+        else begin
+            golden_complete = 1;
+            golden_err_msg = No_Err;
+            new_dram_data[63:52] = dram_black_tea + IND_supply.black_supply;
+            new_dram_data[51:40] = dram_green_tea + IND_supply.green_supply;
+            new_dram_data[39:32] = date_today.month_id;
+            new_dram_data[31:20] = dram_milk + IND_supply.milk_supply;
+            new_dram_data[19:8]  = dram_pineapple + IND_supply.pineapple_supply;
+            new_dram_data[7:0]   =  date_today.day_id;
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 7] = new_dram_data[63:56];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 6] = new_dram_data[55:48];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 5] = new_dram_data[47:40];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 4] = new_dram_data[39:32];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 3] = new_dram_data[31:24];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 2] = new_dram_data[23:16];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 + 1] = new_dram_data[15:8];
+            golden_DRAM[65536 + addr_DRAM.box_id*8 ] = new_dram_data[7:0];
+        end
+    end
+
+    else begin
+        if( {date_today.month_id,date_today.day_id} > {dram_month[3:0],dram_day[4:0]}) begin
+            golden_complete = 0;
+             golden_err_msg = No_Exp;
+         end    
+         else begin
+             golden_complete = 1;
+             golden_err_msg = No_Err;
+         end
+    end
+end endtask
+
+task wait_valid;begin
+    latency = 0;
+    while(inf.out_valid !== 1)begin
+        if(latency == 1000) begin
+            $fatal(1,"latency over 1000 cycles");
+        end
+        
+        latency = latency + 1;
+        @(negedge clk);
+    end
+    
+end endtask
+
+task check_ans;begin
+    if(inf.out_valid === 1) begin
+            if(inf.err_msg !== golden_err_msg || inf.complete !== golden_complete) begin
+                $fatal(1,"golden_err_msg:%0d, golden_complete:%0d; yours err_msg:%0d, complete:%0d|address :%4d",golden_err_msg,golden_complete,inf.err_msg,inf.complete,addr_DRAM.box_id);
+            end
+    end
+    @(negedge clk);
+    if(inf.out_valid !== 0)
+        $fatal(1,"out_valid longer than one  cycle");
+
+end endtask
+
+
+
+endprogram
+
+
+
+
+
+
---
+++ Lab08/PATTERN/TA_PATTERN/Lab08_TA_Pattern/PATTERN_bridge.sv
@@ -0,0 +1,74 @@
+/*
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+NYCU Institute of Electronic
+2024 Spring IC Design Laboratory 
+Lab08: SystemVerilog Design and Verification 
+File Name   : PATTERN_bridge.sv
+Module Name : PATTERN_bridge
+Release version : v1.0 (Release Date: Apr-2024)
+Author : Jui-Huang Tsai (erictsai.ee12@nycu.edu.tw)
+//   (C) Copyright Laboratory System Integration and Silicon Implementation
+//   All Right Reserved
+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+*/
+
+`include "Usertype_BEV.sv"
+
+program automatic PATTERN_bridge(input clk, INF.PATTERN_bridge inf);
+
+`protected
+>YDIN+g@L=?0e?D<3DdMC(VfB]IbX3c?bL:GD#0;)D)/CNgMLL_[-)d-@W?)f;?W
+6CW0c7-6?fV=)9M(3CJUa.G/0)/WA9M?eMXgLH8<E-SZ;>Z>Ze+-U:5[80Fa&3S]
+<XId.S;4#./6H,14ae2Vg?B&4LSI^?,U6.^5I7A7W5Y#UF:>D5/a;b;.#_#8+R>S
+f/)J>JS1E>d-aW>.((=N/K>E4,2E1,gR-g0?QYTWNN6\&#I1^T,Za@^4??GN#+(_
+bONW77bT#7>OE3g(2#1Ia_/X,fG27HS?fG,_UaP#W:^[M/(#K#OcP<S([00+PBaS
+@f56XQ[UE+/TL+^[3]7-KK>SDIZLBB8UECZK_,.[g5]TNME8OL[ASU136:KH]B)B
+@0ZLY<F^C&C>&U[7.JI#1/3HBT-=#4Q5g&D+S5]=Jc[^ab=ITb,B?>bL(.+1C#.Y
+Mg9N)&Y3b(U@URKM\b-_TdVe./,eROU[1fAHU=+I2F(3]c;2RgYa/[Z^7Z5eP7-O
+NFZN#Z#Lg[=\;b=8)_@_DMDE5S&7M?OVCHP_>bXE^U/>e78EYDWeZN4M)#X)O67g
++4e8D^^WA(BF?/0/THDXH+E<]MGR)cIMNULPX3P_;<g@MG^7/F+</8:5ZMZOGF#3
+c[9.LO_]><PNF0;e\#3B3@?8^Fb[O2?JPSa5E;\=FEbV^N^N\Z,/_K9M&,T)S3?)
+._U<\H&2G3MgX3C:\e;SAL[F=CdSAZ3@[VQ:XCLCBE8=S@L+bfA^<,XO#fZ@aUeM
+E,N77f:UC[+0;A)]bYS;>(E1VRf?A54d(VDH7X&A\gZE=X))Q9Q[W=(HMXc<MLXQ
+[F>_Wd?aD-=X(]0T6BMH+)ZE/eZD+WKb:c#8[/&:3]<1#/7\7CR,GLCe&))&/c5f
+-bYQ1BD/=^Eab?Y.H+^BB1d[LS-_a\/3>2c,ASNL;S7E[B=_4_#ZD+fLYT-)&VD7
+Xb40?7:<YHgSB19:V;)5__.DW.J]5U2>;Y69JSLVV7E)&U=C;T+@?KYN?Y&@cP3J
+B[R#BeaY(06.+Q&;dF8fZBFbZ_3+[)20fge+Ic1)a]NEgP=T_P/R2\>eDY8Q\#W0
+-)34HC,76Ig+JUVU.5K5.B[8AA(cfP28f98LC4?EQO?-BdVU:-UX0A4X@N>N^D#U
+[?9HG+-^#(gZ)3[G,@^Q>4\,.D>#O<)5@PR5d7e;V_I/PfAc5]5YAbI6UU<U@(+[
+2eVM#0?+G:e>CSIP=Ta1C:]W?M#Ka]5G@WV03ca#]9P<BZc@><&Rb38TC(\3B)C#
+a[]T,17\(R3Wa&,HZ>VcCFIS/JYdMI06bEOQ&\VZ&G5aR2#Of\U.0,#T16E(@=A8
+3XJ4\;H.GW56gE1QP2U?E)cdPSH(?U+Xefe)?)]_8+B^CXDNVZ(E=/9U\a;9\I,]
+HR4@[@07(T65EK<-U-[\SEVNP,+RX)&&,=2]413^M32dZ&7e?^_4fD<0M1&57;KA
+I/14P?d=53;<GW4MDY[=.W.0NTI_<c<C7R344EZ.;F7F/:H#PeVS7&I0ge6C&+:9
+M57;VCC<BBg20UGR5^0)6GNADE5J593QMQ9ZKYCbMAgG>(Z-@(2:2A>>=#cZ1UM,
+HDH/Y<F?W4O;E@bJZR)IM8^E3]OHE5bG;SP.RUGUSHJW5f&.;93KUZ4Z@H76G>\S
+=9)M(74<&ZCUX<I-7?1@&[X)b\>eb1E[-&S0+UF3=fCS[1H=f2V[YZN9a1T5(O3;
+IS)f4g>C?cL_LYIKM=gGJ);)DfUCN(JSE=TgCI?A_2R#/f[?-3;DW_=MOKJ\7EK<
++J4a_&S-[7[GR\W#&4#4fL1,8M0b)U:TJ)fD)<_<SSMdVSMQ\e6,97\0#OYdVLQE
+bgOa=A2:ZPW(VT5OHV:Y4+N\@37^,Jg+[]DYO;c2SIOPARg#Q#DN:_=(C/H1ZUB9
+UJU-J)/b:5[bgc.EC:0V&E;Ug/a(JK\1.cZ63E.A8/>A9_RJ;C_5ED]VY[;GSNRO
+g+DG/M.J5D6gVgC[I.XK=IB+E3Da++;bQ6Cb:-ZAZB^P(6U?Tc[_0L.fNP+X?P3(
+d80NgOc<@>O8e:I:MM/?MZFGVA1G6^(DDBcYX9S,86QJc7+\H-KNJYW16CYC(5X7
+B0MY6:e;2VL3>Q)B>OfHd<]4+Y]LRLU]NF#B[>L:KQ0F0OGPX0FS<Hb<Ef#BDFIR
+A7d8dF0.S,-ANe+[PVeEb6SKE3EMc^=V(+7CK8/_TJ^6_>W9]W@a\ed5,(cD?F.:
+X4<MVP_4+W1d_/CcPZ)]+3>bJOH195,:<\+#SJ_U)K^Fac==6EU&4DX8I\M@U(D\
+W4fcTADgd4)6/-T-4D)D#1P^BW4AGPJ,V-1CRM:SOc4V1G6d,Y9=QE?(agF7DI1T
+-EV-&DGZA7I<2XWV;K-1RFZ?HgET,fSKB@2Z97HL4;);]Ea27ISPG67,CT969e=_
+f]Z=e<)P8<3JSa_\,eNX^&:3H]H;O9B/?ZE]&4aR@4YW_-95&?73cAMGB8;)@H>A
+&200FPFPecdMS8I[W(VZ1?IXP=]2NgV#577C@A/N/_3SEf-HC:d0M[5=ee=+;1Q?
+M<UK&ZdKBKe9)F0dfK>78+<=21BRF/3J)0BCY\_+C9P392U06HZ\E:^=WSI#c060
+.e8CKX?L2Eaf3+(d7B/84]&KEV,N\U-5eI9a<?@_Rg(#^0=#gM;O<IBN0?8;RDT5
+1L5ZZE3[L[)QDL@]Sa5MFZ80R?[@d.+R=[@G5>9+?TS@S\_O.6e:4S>FNIU9:Ae#
+=aG5N@+&X9X,V3)#0@G0\\J4,W(@P/3VLZBPKGEF-OdCAQH;L]?CJ5PP(?P6cKTF
+Z2d0<W>bDC1eP83TE,(TEBd+74@G03=M)aWbTT8NY&9R5D_LJI=]26F2c([cH?MG
+4BA_SfO;)WC+G4[UG\,<d].4ASH6Q@K0e]0].e.P,^EF+\4ETF>KS,_f?WS(L=aP
+,D?KW;)A^,C4g^G4]M_YB\\#Y)B13?J#;.^OL+<)?5EYb+2@^:?ZeO3gM3B+)Y5d
+b5X(MM60O,CI^D>ZL/GLJ\fQfN+>Ecd>5][ZBTL#TA<7gQN&g5Eg;T+[f0K1GB\M
+B=@UVLOQ]?QP?f8??Ee+MF.RVHMKZX/(aX#?WT,0><C:E#G^cGJ5/=4EEY0@M@c/
+FC,ZDK:-&L)-YSFLCaJ#O?;>XHL.#]4\gR3d9DIG)9B)T0TB(GG6[/OX=&Ug(bFC
+FT_NFLZdbFCa6SgGM7JfJ,G?1^R+d]JJ>+.>=b#<+4@)[3;TJcfa-,3FBNW)R=8I
+7<Z:83O^=M?J0MQG#V-+A&#P73_ZF3,IGL/;I4,Da_BR=W4OUOg04f/V)P2g)c_=
+DeeTWK)?g@8?R^C5V2DCJBT:7:JC@8.,)\OS)JVW06BMbMd2>1;aD9gN)_H,NJcN
+V\Pe9+fWgHb?.$
+`endprotected
